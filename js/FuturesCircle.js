99795c1a86eadcf24e1edffba9c4326f8763cf5d
wiener0zyj <445892688@qq.com>
2016-04-06 09:20:26 +0800

    Signed-off-by: wiener0zyj <445892688@qq.com>

---
+++ js/FuturesCircle.js
@@ -0,0 +1,29 @@
+﻿$(document).ready(function () {
+    //tab单击事件
+    clickADrop();
+});
+
+//tab单击事件
+function clickADrop() {
+    $(".a_drop").click(function () {
+        var idNum = $(this).attr("data-title");
+        if (idNum == "hottopic")
+        {
+            if ($("#hottopicdiv").hasClass("a_droped")) {
+                //do nothing
+            } else {
+                $("#hottopicdiv").addClass("a_droped");
+                $("#freetopicdiv").removeClass("a_droped");
+            }
+        } else
+        {
+            if ($("#freetopicdiv").hasClass("a_droped")) {
+                //do nothing
+            }
+            else {
+                $("#freetopicdiv").addClass("a_droped");
+                $("#hottopicdiv").removeClass("a_droped");
+            }
+        } 
+    });
+}
